0cce3a3551b073e5b55009bf2f45820ed4aa6b8b
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2023-06-02 16:07:04 +0200

    add CRUD

--- {{ cookiecutter.project_slug }}/apps/{{ cookiecutter.app_name }}/urls.py
+++ {{ cookiecutter.project_slug }}/apps/{{ cookiecutter.app_name }}/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from .views import (
+    {{ cookiecutter.app_name }}CreateView,
+    {{ cookiecutter.app_name }}UpdateView,
+    {{ cookiecutter.app_name }}DeleteView,
+    {{ cookiecutter.app_name }}DetailView,
+    {{ cookiecutter.app_name }}ListView,
+)
 
 app_name = "{{ cookiecutter.app_name }}"
 
 urlpatterns = [
-    path("", name="{{ cookiecutter.app_name }}"),
+    path("", {{ cookiecutter.app_name }}ListView.as_view(), name="{{ cookiecutter.app_name }}_list"),
+    path("create/", {{ cookiecutter.app_name }}CreateView.as_view(), name="{{ cookiecutter.app_name }}_create"),
+    path("<int:pk>/", {{ cookiecutter.app_name }}DetailView.as_view(), name="{{ cookiecutter.app_name }}_detail"),
+    path("<int:pk>/update/", {{ cookiecutter.app_name }}UpdateView.as_view(), name="{{ cookiecutter.app_name }}_update"),
+    path("<int:pk>/delete/", {{ cookiecutter.app_name }}DeleteView.as_view(), name="{{ cookiecutter.app_name }}_delete"),
 ]
--- {{ cookiecutter.project_slug }}/apps/{{ cookiecutter.app_name }}/views.py
+++ {{ cookiecutter.project_slug }}/apps/{{ cookiecutter.app_name }}/views.py
@@ -1,9 +1,27 @@
 from models import {{ cookiecutter.app_name }}
 from forms import {{ cookiecutter.app_name }}Form
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
 
-
-class BookCreateView(CreateView):
+class {{ cookiecutter.app_name }}CreateView(CreateView):
     model = {{ cookiecutter.app_name }}
-    template_name = "{{ cookiecutter.app_name }}/add_book.html"
+    template_name = "{{ cookiecutter.app_name }}/create_{{ cookiecutter.app_name }}.html"
     form_class = {{ cookiecutter.app_name }}Form
+
+class {{ cookiecutter.app_name }}UpdateView(UpdateView):
+    model = {{ cookiecutter.app_name }}
+    template_name = "{{ cookiecutter.app_name }}/update_{{ cookiecutter.app_name }}.html"
+    form_class = {{ cookiecutter.app_name }}Form
+
+class {{ cookiecutter.app_name }}DeleteView(DeleteView):
+    model = {{ cookiecutter.app_name }}
+    template_name = "{{ cookiecutter.app_name }}/delete_{{ cookiecutter.app_name }}.html"
+    success_url = "/"  # Rediriger vers l'URL souhaitée après la suppression
+
+class {{ cookiecutter.app_name }}DetailView(DetailView):
+    model = {{ cookiecutter.app_name }}
+    template_name = "{{ cookiecutter.app_name }}/detail_{{ cookiecutter.app_name }}.html"
+
+class {{ cookiecutter.app_name }}ListView(ListView):
+    model = {{ cookiecutter.app_name }}
+    template_name = "{{ cookiecutter.app_name }}/list_{{ cookiecutter.app_name }}.html"
+
